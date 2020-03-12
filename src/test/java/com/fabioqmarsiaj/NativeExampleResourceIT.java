efdbb89d31261680d18ff7978d3ab9c9558b2538
Fabio Marsiaj <fabiomarsiaj@gmail.com>
2020-03-12 14:08:48 -0300

    first

---
+++ src/test/java/com/fabioqmarsiaj/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.fabioqmarsiaj;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
